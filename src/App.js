02a8f98d1a6a0ae637f20bf30d825857a8479250
Long <17272827+longb1@users.noreply.github.com>
2022-06-22 13:00:42 +0100

    added handlesubmit function
    handlesubmit function to add a task to task list when submit button
    clicked.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
 import React from "react";
 import Overview from './components/Overview.js';
 
 function App() {
   const [tasks, setTasks] = React.useState([])
 
+  function handleSubmit(event){
+    const value = event.target
+    event.preventDefault()
+
+    setTasks(prevTasks => [
+      ...prevTasks, value
+    ])
+
+    return <Overview tasks={tasks}/>
+  }
+
   return (
     <div>
-      <form>
-        <label for="fname">First name:</label><br/>
-        <input type="text" id="fname" name="fname"/><br/>
+      <form onSubmit={handleSubmit}>
+        <label for="fname">Add a task</label><br/>
+        <input  type="text" 
+                id="fname" 
+                name="fname"
+        /><br/>
 
         <button>Submit</button>
       </form>
