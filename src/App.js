9dc50a79a1161cb822fd41c28545a0adccfb4f4d
Long <17272827+longb1@users.noreply.github.com>
2022-06-22 08:15:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React TODAY
         </a>
       </header>
     </div>
