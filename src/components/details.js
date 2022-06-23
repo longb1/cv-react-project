c92f396a496b22052d6fff5e376febfc5833db38
long <17272827+longb1@users.noreply.github.com>
2022-06-23 23:02:37 +0100

    add state for details for cv application
    add state for sections in the CV

---
+++ src/components/details.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function Details(){
+    const [personal, setPersonal]=React.useState(
+        {
+            name:"",
+            email:"",
+            phone:""
+        }
+    )
+
+    const [education, setEducation]=React.useState([
+        {}
+    ])
+
+    const [work, setWork]=React.useState([
+        {}
+    ])
+
+    return(
+
+    )
+}
