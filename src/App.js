2378ce04b36c070e62b0b28e5315c88673e4e84a
long <17272827+longb1@users.noreply.github.com>
2022-06-23 22:36:48 +0100

    reset for new project
    reset for cv project

--- src/App.js
+++ src/App.js
@@ -2,34 +2,7 @@ import React from "react";
 import Overview from './components/Overview.js';
 
 function App() {
-  const [tasks, setTasks] = React.useState([])
-  const [text, setText] = React.useState('')
-
-  function handleSubmit(event){
-    event.preventDefault()
-    setTasks([...tasks, {
-      id:tasks.length,
-      value:text
-    }])
-    setText('')
-  }
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input  type="text"
-                placeholder="enter task"
-                onChange={e => setText(e.target.value)}
-                value={text}
-        /><br/>
-
-        <button>Submit</button>
-      </form>
-      <Overview array={tasks} />
-    
-    </div>
-    
-  );
+  
 }
 
 export default App;
