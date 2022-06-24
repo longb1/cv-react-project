b9b38128204963fdacdcb8ab180666644841d8ed
long <17272827+longb1@users.noreply.github.com>
2022-06-24 08:11:16 +0100

    split into components
    split each CV section into components for better maintainbability

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from "react";
-import Overview from './components/Overview.js';
+import Education from "./components/Education.js";
+import Personal from "./components/personal.js";
+import Experience from "./components/Experience.js";
 
 function App() {
   
+    const [personal, setPersonal]=React.useState(
+        {
+            name:"",
+            email:"",
+            phone:""
+        }
+    )
+
+    const [education, setEducation]=React.useState([
+        {
+            schoolName:"",
+            subject:"",
+            dateStart:""
+        }
+    ])
+
+    const [work, setWork]=React.useState([
+        {
+            companyName:"",
+            position:"",
+            summary:"",
+            dateFrom:"",
+            dateUntil:""
+        }
+    ])
+
+    return(
+        <main>
+            <Personal />
+            <Education />
+            <Experience />
+        </main>
+    )
 }
 
 export default App;
