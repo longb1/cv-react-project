3d0acface4fd16534db65443d5884b0700688c23
long <17272827+longb1@users.noreply.github.com>
2022-07-02 23:01:48 +0100

    add existing experiences to form section

--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Experience from "./experience.js";
+import Experience from "./experienceForm.js";
+import ExperienceFormSection from "./experienceFormSection.js";
 import Personal from "./personal.js";
 
 export default function Form(props) {
@@ -8,6 +9,7 @@ export default function Form(props) {
     return(
         <div id="form">
             <Personal cvTemplate={props.cvTemplate} handleChange={props.handleChangePersonal}/>
+            <ExperienceFormSection allExperiences={props.allExperiences}/>
             <Experience  
                 cvTemplate={props.cvTemplate} 
                 handleChange={props.handleChangeExperience}
