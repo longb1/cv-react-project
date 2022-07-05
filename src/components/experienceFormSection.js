6dd303943fb27756184f5466e1dd0e24c65f3046
long <17272827+longb1@users.noreply.github.com>
2022-07-05 23:18:46 +0100

    add styling

--- src/components/experienceFormSection.js
+++ src/components/experienceFormSection.js
@@ -4,11 +4,18 @@ export default function ExperienceFormSection(props){
     const allExp=props.allExperiences
 
     const formattedExperiences = allExp.map(exp =>{
-        return <li key={exp.id}>{exp.summary}</li>;
+        return <li key={exp.id}>
+            <p>{exp.companyName}</p>
+            <p>{exp.position}</p>
+            <p>{exp.summary}</p>
+            <p>{exp.dateFrom} to {exp.dateUntil}</p>
+            <button /*onClick={deleteFunction} */>edit</button>
+            <button>remove</button>
+        </li>;
     })
 
     return(
-        <ul>
+        <ul id="formExperiences">
             {formattedExperiences}
         </ul>
 
