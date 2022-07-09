823c062d76a1f12e63d73437c1839fad441f6ea3
long <17272827+longb1@users.noreply.github.com>
2022-07-09 22:34:51 +0100

    change how list item is returned
    return list item instead of one whole unordered list - allows me to use
    conditional rendering on each item from teh form component.

--- src/components/experienceFormSection.js
+++ src/components/experienceFormSection.js
@@ -1,25 +1,15 @@
 import React from "react"
-import EditForm from "./editForm.js";
 
 export default function ExperienceFormSection(props){
-    const allExp=props.allExperiences
 
-    const formattedExperiences = allExp.map(exp =>{
-        return <li key={exp.id}>
-            <p>{exp.companyName}</p>
-            <p>{exp.position}</p>
-            <p>{exp.summary}</p>
-            <p>{exp.dateFrom} to {exp.dateUntil}</p>
+    return (
+        <li key={props.allExp.id}>
+            <p>{props.allExp.companyName}</p>
+            <p>{props.allExp.position}</p>
+            <p>{props.allExp.summary}</p>
+            <p>{props.allExp.dateFrom} to {props.allExp.dateUntil}</p>
             <button /*onClick={deleteFunction} */>edit</button>
             <button>remove</button>
-            <EditForm/>
-        </li>;
-    })
-
-    return(
-        <ul id="formExperiences">
-            {formattedExperiences}
-        </ul>
-
+        </li>
     )
 }
