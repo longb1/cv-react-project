196916bb1401dd6865c3881fc7ebdab9be398ef0
long <17272827+longb1@users.noreply.github.com>
2022-07-23 00:35:44 +0100

    add single source of truth

--- src/components/editableForm.js
+++ src/components/editableForm.js
@@ -9,30 +9,40 @@ export default function EditableForm(props) {
             alt="experience"
             name="companyName"
             placeholder="company"
+            onChange={props.handleEditFormChange}
+            value={props.editFormData.companyName}
             />
             <input type="text" 
             alt="experience"
             name="position"
             placeholder="position"
+            onChange={props.handleEditFormChange}
+            value={props.editFormData.position}
             />
             <textarea
             alt="experience"
             name="summary"
             placeholder="summary"
+            onChange={props.handleEditFormChange}
+            value={props.editFormData.summary}
             />
             <label htmlFor="dateFrom">work from</label>
             <input type="date" 
             alt="experience"
             name="dateFrom"
             placeholder="dateFrom"
+            onChange={props.handleEditFormChange}
+            value={props.editFormData.dateFrom}
             />
             <label htmlFor="dateUntil">work until</label>
             <input type="date" 
             alt="experience"
             name="dateUntil"
             placeholder="dateUntil"
+            onChange={props.handleEditFormChange}
+            value={props.editFormData.dateUntil}
             />
-            <button onClick={(event)=>props.handleEditFormChange(event)}>save</button>
+            <button>save</button>
             <button onClick={(event)=>props.handleCancel(event)}>cancel</button>
         </form>
     )
